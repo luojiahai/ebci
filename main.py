9bbc78107d2ec87966aba07def4bcbba5ff254d1
JEFFREY <hoipck@gmail.com>
2019-05-22 07:52:56 +0000

    update

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def tabular_driver(dataset, gamma):
                           categorical_names=dataset.categorical_names)
 
     for i in range(10):
-        instance = dataset.validation[0]
+        instance = dataset.validation[i]
         fact, contrast = interpreter.interpret(instance=instance, 
                                             class_label=clf.predict([instance])[0], 
                                             predict_fn=clf.predict_proba,
@@ -26,12 +26,14 @@ def tabular_driver(dataset, gamma):
         for k, v in {'subject': instance, 'fact': fact, 'contrast': contrast}.items():
             class_label = clf.predict([v])[0]
             predict_proba = clf.predict_proba([v])[0]
-            contents = [f"{k} feature_values {v} class_label {class_label} predict_proba {predict_proba}"]
+            values = []
+            for e in v: values.append(int(e))
+            contents = [f"{k} feature_values {str(values)} class_label {class_label} predict_proba {predict_proba}"]
             contents.append(f"prediction: {dataset.class_names[clf.predict([v])[0]]}")
-            for i in range(len(v)):
-                feature_name = dataset.feature_names[i]
-                value = int(v[i])
-                categorical_name = dataset.categorical_names[i][value]
+            for j in range(len(v)):
+                feature_name = dataset.feature_names[j]
+                value = int(v[j])
+                categorical_name = dataset.categorical_names[j][value]
                 contents.append(f"\t{feature_name}: {categorical_name}")
             utilities.Debug.log(contents=contents)
             
@@ -49,7 +51,7 @@ def main():
                                   dataset.categorical_names],
                         pformat=True)
     utilities.Debug.log(contents=['\n'])
-    tabular_driver(dataset, 0.7)
+    tabular_driver(dataset, 0.5)
     
 
 if __name__ == "__main__":
